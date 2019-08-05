c411eabd08a38996773551b1c243843703102a95
胡晴风 <1397649957@qq.com>
2019-08-05 11:29:46 +0800

    lombok

--- src/test/java/org/flux/webflux/StreamTest.java
+++ src/test/java/org/flux/webflux/StreamTest.java
@@ -1,6 +1,11 @@
 package org.flux.webflux;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
 import java.util.*;
+import java.util.function.Function;
 import java.util.stream.Collector;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
@@ -10,38 +15,28 @@ import java.util.stream.Stream;
  * @date 2019-08-01
  */
 public class StreamTest {
+    @AllArgsConstructor
+    @Setter
+    @Getter
+    static class Person {
+        String name;
+        String value;
+    }
     public static void main(String[] args) {
-
-        class Person{
-            String name;
-            String value;
-
-            public Person(String name, String value) {
-                this.name = name;
-                this.value = value;
-            }
-        }
-
-        Map collect = Arrays.asList(
+        Stream<Person> personStream = Stream.of(
                 new Person("哈哈", "ddd"),
                 new Person("ddd", "bb"),
                 new Person("ddd", "哈哈")
-        ).stream()
+        );
+        Map<String, List<String>> maps = maps(personStream, Person::getName, Person::getValue);
+        System.out.println(maps);
+    }
 
-                .collect(Collector.<Person, Map<String,Set<String>>>of(HashMap::new, (x, y) -> {
-                    Set<String> values = x.computeIfAbsent(y.name, (key) -> {
-                        Set<String> objects = new HashSet<>();
-                        objects.add(y.value);
-                        return objects;
-                    });
-                    values.add(y.name);
-                    x.put(y.name,values);
-                }, (x, y) -> {
-                    System.out.println();
-                    return x;
-                }));
-
-        System.out.println(collect);
-
+    public static <K,V,S> Map<K,List<V>> maps(Stream<S> stream, Function<S,K> key,Function<S,V> value){
+        return stream.collect(Collector.<S,Map<K,List<V>>>of(HashMap::new,(map,element)->{
+            K mapKey = key.apply(element);
+            List<V> vs = map.computeIfAbsent(mapKey, k -> new ArrayList<>());
+            vs.add(value.apply(element));
+        },(x,y)->x));
     }
 }
