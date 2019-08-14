17fe9f20000741d6e7d2ffd56e98b444cbf1b50d
胡晴风 <1397649957@qq.com>
2019-08-14 16:22:03 +0800

    co

--- src/main/java/org/web/WebfluxApplication.java
+++ src/main/java/org/web/WebfluxApplication.java
@@ -15,14 +15,9 @@ import java.util.Map;
 @SpringBootApplication
 public class WebfluxApplication {
 
-  public void show1(){}
-
   public static void main(String[] args) throws Exception {
     SpringApplication.run(WebfluxApplication.class, args);
-    String s = "";
   }
-
-  public void show(){}
 
   @Bean
   public HandlerMapping handlerMapping() {
